7c2a238ba3bb4ab883b8f93f549de3682195caf3
aixpec <aixpec@gisystemsint.com>
2016-11-09 11:26:37 -0500

    Initial Commit

---
+++ Recorrido/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recorrido
+//
+//  Created by Andrés Ixpec on 9/11/16.
+//  Copyright © 2016 Andrés Ixpec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
